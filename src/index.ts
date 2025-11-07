6eac464f2a1180434db7e34ed0de4a367ec22182
shangxueink <1919892171@qq.com>
2025-11-07 10:45:58 +0800

    初始化

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema, Logger } from 'koishi';
+import { Context, Schema, Logger, Session, h } from 'koishi';
 import { } from 'koishi-plugin-adapter-iirose';
 
 export const name = 'iirose-follow';
@@ -34,69 +34,78 @@ export function apply(ctx: Context, config: Config) {
     primary: 'uid'
   });
 
-  ctx.command('跟随', '让机器人跟随我！').action(async v => {
-    const uid = v.session.author.userId;
-    if (config.permission.indexOf(uid) < 0) { return ' [IIROSE-Follow] 权限不足，请在控制台处将你的唯一标识添加到允许列表'; }
+  ctx.command('iirose.跟随', '让机器人跟随我！')
+    .action(async ({ session }) => {
+      const uid = session.userId;
+      if (config.permission.indexOf(uid) < 0) {
+        return '[IIROSE-Follow] 权限不足，请在控制台处将你的唯一标识添加到允许列表';
+      }
 
-    if (v.session.platform !== 'iirose') { return ' [IIROSE-Follow] 该平台不支持使用此插件'; }
-    const userData = await ctx.database.get('iirose_follow', v.session.author.userId);
+      if (session.platform !== 'iirose') {
+        return '[IIROSE-Follow] 该平台不支持使用此插件';
+      }
+      const userData = await ctx.database.get('iirose_follow', session.userId);
 
-    if (userData.length > 0 && userData[0].status) {
-      return ' [IIROSE-Follow] 你已经设置为跟随状态了哦~';
-    } else if (userData.length > 0) {
-      await ctx.database.set('iirose_follow', v.session.author.userId, {
-        status: true
-      });
+      if (userData.length > 0 && userData[0].status) {
+        return '[IIROSE-Follow] 你已经设置为跟随状态了哦~';
+      } else if (userData.length > 0) {
+        await ctx.database.set('iirose_follow', session.userId, {
+          status: true
+        });
 
-      return ` [IIROSE-Follow] 将 [*${v.session.author.username}*] 设置为BOT跟随状态`;
-    } else if (userData.length == 0) {
-      ctx.database.create('iirose_follow', {
-        uid: v.session.author.userId,
-        status: true
-      });
+        return `[IIROSE-Follow] 将 ${h.at(session.userId)} 设置为BOT跟随状态`;
+      } else if (userData.length == 0) {
+        ctx.database.create('iirose_follow', {
+          uid: session.userId,
+          status: true
+        });
 
-      return ` [IIROSE-Follow] 将 [*${v.session.author.username}*] 设置为BOT跟随状态`;
-    }
-  });
+        return `[IIROSE-Follow] 将 ${h.at(session.userId)} 设置为BOT跟随状态`;
+      }
+    });
 
-  ctx.command('取消跟随', '停止机器人的跟随！').action(async v => {
-    if (v.session.platform !== 'iirose') { return ' [IIROSE-Follow] 该平台不支持使用此插件'; }
-    const userData = await ctx.database.get('iirose_follow', v.session.author.userId);
+  ctx.command('iirose.取消跟随', '停止机器人的跟随！')
+    .action(async ({ session }) => {
+      if (session.platform !== 'iirose') {
+        return '[IIROSE-Follow] 该平台不支持使用此插件';
+      }
+      const userData = await ctx.database.get('iirose_follow', session.userId);
 
-    if (userData.length > 0 && !userData[0].status) {
-      return ` [IIROSE-Follow]  [*${v.session.author.username}*] 你本就没有要求BOT跟随`;
-    } else if (userData.length > 0) {
-      await ctx.database.set('iirose_follow', v.session.author.userId, {
-        status: false
-      });
+      if (userData.length > 0 && !userData[0].status) {
+        return `[IIROSE-Follow]  ${h.at(session.userId)} 你还没有要求BOT跟随过哦~无需取消。`;
+      } else if (userData.length > 0) {
+        await ctx.database.set('iirose_follow', session.userId, {
+          status: false
+        });
 
-      return ` [IIROSE-Follow] 将 [*${v.session.author.username}*] 设置BOT取消跟随啦`;
-    } else if (userData.length == 0) {
-      ctx.database.create('iirose_follow', {
-        uid: v.session.author.userId,
-        status: false
-      });
+        return `[IIROSE-Follow] 将 ${h.at(session.userId)} 设置BOT取消跟随啦`;
+      } else if (userData.length == 0) {
+        ctx.database.create('iirose_follow', {
+          uid: session.userId,
+          status: false
+        });
 
-      return ` [IIROSE-Follow] 将 [*${v.session.author.username}*] 设置BOT取消跟随啦`;
-    }
+        return `[IIROSE-Follow] 将 ${h.at(session.userId)} 设置BOT取消跟随啦`;
+      }
 
-  });
+    });
 
-  ctx.on('iirose/switchRoom', async (session, data) => {
+  ctx.on('iirose/switchRoom', async (session: Session, data) => {
     const userData = await ctx.database.get('iirose_follow', data.uid);
-    if (userData.length <= 0 || !userData[0].status) { return; }
+    if (userData.length <= 0 || !userData[0].status) {
+      return;
+    }
 
     const newRoomId = data.targetRoom;
     if (session.guildId === newRoomId) {
-      return session.bot.sendMessage(`private:${data.uid}`, ' [IIROSE-Follow] 跳转失败，目标房间与当前机器人所在房间可能位置相同')
+      await session.bot.sendMessage(session.channelId, '[IIROSE-Follow] 跳转失败，目标房间与当前机器人所在房间可能位置相同')
+      return
     }
-
-    session.bot.internal.moveRoom({ roomId: data.targetRoom });
-    // ctx.emit('iirose/moveRoom', { roomId: data.targetRoom });
-    logger.info(`跳转到房间ID为:[ ${data.targetRoom} ]的房间`);
+    logger.info(`跟随 ${session.username} 跳转到房间: ${data.targetRoom}`);
+    await session.bot.internal.moveRoom({ roomId: data.targetRoom });
   });
 
-  ctx.once('iirose/selfMove', (session, data) => {
-    session.bot.internal.moveRoom({ roomId: data.id });
-  });
+  // ctx.once('iirose/selfMove', (session, data) => {
+  //   session.bot.internal.moveRoom({ roomId: data.id });
+  // });
 }
