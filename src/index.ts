0363d4333a43e6c231dd0a9d6b745a2bb5cef5ee
shangxueink <1919892171@qq.com>
2025-11-09 22:51:22 +0800

    bump

--- src/index.ts
+++ src/index.ts
@@ -18,13 +18,14 @@ export const usage = `
 
 ---
 
-当适配器下发 iirose/switchRoom 事件后，机器人会根据设定的UID前往目标房间
+当适配器下发 iirose/guild-member-switchRoom 事件后，机器人会根据设定的UID前往目标房间
 
 ---
-`
+`;
 
 
-export interface Config {
+export interface Config
+{
   permission: string[];
 }
 
@@ -35,17 +36,20 @@ export const Config: Schema<Config> = Schema.object({
 const logger = new Logger('IIROSE-Follow');
 
 declare module 'koishi' {
-  interface Tables {
+  interface Tables
+  {
     iirose_follow: follow;
   }
 }
 
-export interface follow {
+export interface follow
+{
   uid: string;
   status: boolean;
 }
 
-export function apply(ctx: Context, config: Config) {
+export function apply(ctx: Context, config: Config)
+{
   ctx.model.extend('iirose_follow', {
     uid: 'string',
     status: 'boolean'
@@ -54,26 +58,32 @@ export function apply(ctx: Context, config: Config) {
   });
 
   ctx.command('iirose.跟随', '让机器人跟随我！')
-    .action(async ({ session }) => {
+    .action(async ({ session }) =>
+    {
       const uid = session.userId;
-      if (config.permission.indexOf(uid) < 0) {
+      if (config.permission.indexOf(uid) < 0)
+      {
         return '[IIROSE-Follow] 权限不足，请在控制台处将你的唯一标识添加到允许列表';
       }
 
-      if (session.platform !== 'iirose') {
+      if (session.platform !== 'iirose')
+      {
         return '[IIROSE-Follow] 该平台不支持使用此插件';
       }
       const userData = await ctx.database.get('iirose_follow', session.userId);
 
-      if (userData.length > 0 && userData[0].status) {
+      if (userData.length > 0 && userData[0].status)
+      {
         return '[IIROSE-Follow] 你已经设置为跟随状态了哦~';
-      } else if (userData.length > 0) {
+      } else if (userData.length > 0)
+      {
         await ctx.database.set('iirose_follow', session.userId, {
           status: true
         });
 
         return `[IIROSE-Follow] 将 ${h.at(session.userId)} 设置为BOT跟随状态`;
-      } else if (userData.length == 0) {
+      } else if (userData.length == 0)
+      {
         ctx.database.create('iirose_follow', {
           uid: session.userId,
           status: true
@@ -84,21 +94,26 @@ export function apply(ctx: Context, config: Config) {
     });
 
   ctx.command('iirose.取消跟随', '停止机器人的跟随！')
-    .action(async ({ session }) => {
-      if (session.platform !== 'iirose') {
+    .action(async ({ session }) =>
+    {
+      if (session.platform !== 'iirose')
+      {
         return '[IIROSE-Follow] 该平台不支持使用此插件';
       }
       const userData = await ctx.database.get('iirose_follow', session.userId);
 
-      if (userData.length > 0 && !userData[0].status) {
+      if (userData.length > 0 && !userData[0].status)
+      {
         return `[IIROSE-Follow]  ${h.at(session.userId)} 你还没有要求BOT跟随过哦~无需取消。`;
-      } else if (userData.length > 0) {
+      } else if (userData.length > 0)
+      {
         await ctx.database.set('iirose_follow', session.userId, {
           status: false
         });
 
         return `[IIROSE-Follow] 将 ${h.at(session.userId)} 设置BOT取消跟随啦`;
-      } else if (userData.length == 0) {
+      } else if (userData.length == 0)
+      {
         ctx.database.create('iirose_follow', {
           uid: session.userId,
           status: false
@@ -109,20 +124,23 @@ export function apply(ctx: Context, config: Config) {
 
     });
 
-  ctx.on('iirose/switchRoom', async (session: Session, data) => {
+  ctx.on('iirose/guild-member-switchRoom' as any, async (session: Session, data) =>
+  {
     const userData = await ctx.database.get('iirose_follow', data.uid);
-    if (userData.length <= 0 || !userData[0].status) {
+    if (userData.length <= 0 || !userData[0].status)
+    {
       return;
     }
 
     const newRoomId = data.targetRoom;
-    if (session.guildId === newRoomId) {
-      await session.bot.sendMessage(session.channelId, '[IIROSE-Follow] 跳转失败，目标房间与当前机器人所在房间可能位置相同')
-      return
+    if (session.guildId === newRoomId)
+    {
+      await session.bot.sendMessage(session.channelId, '[IIROSE-Follow] 跳转失败，目标房间与当前机器人所在房间可能位置相同');
+      return;
     }
     logger.info(`跟随 ${session.username} 跳转到房间: ${data.targetRoom}`);
     await session.bot.internal.moveRoom({ roomId: data.targetRoom });
-    return
+    return;
   });
 
   // ctx.once('iirose/selfMove', (session, data) => {
